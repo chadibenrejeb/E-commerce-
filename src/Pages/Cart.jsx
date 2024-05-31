743fc6cf6f60878360ee18e81db0a080f651e841
chadibenrejeb <rejebchadi@gmail.com>
2024-05-31 20:12:15 +0100

    main

---
+++ src/Pages/Cart.jsx
@@ -0,0 +1,13 @@
+// Cart.js
+import React from 'react';
+
+const Cart = () => {
+  return (
+    <div>
+      <h1>Your Cart</h1>
+      {/* Cart details code */}
+    </div>
+  );
+};
+
+export default Cart;
---
+++ src/components/Items/Item.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Item.css'
+import { Link } from 'react-router-dom'
+const Item = (props) => {
+  return (
+    <div className='item'>
+      <Link to={`/product/${props.id}`}><img src={props.image} alt="" /></Link>
+      <p>{props.name}</p>
+      <div className="item-prices">
+        <div className="item-price-new">
+            {props.new_price} DNT
+        </div>
+        <div className="item-price-old">
+            {props.old_price} DNT
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Item
